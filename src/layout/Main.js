6807f5638a77eb6f202c53c03a7c711964b75cf9
burgokmen <burgokmen@gmail.com>
2023-08-14 17:50:46 +0300

    layout done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import OrderPage from "../pages/OrderPage";
-import SuccessPage from "../pages/SuccessPage";
+
 import Header from "./Header";
 import PageBody from "./PageBody";
 
@@ -12,17 +9,7 @@ const MainPage = () => {
       <div>
         <Header />
       </div>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/order" exact>
-          <OrderPage />
-        </Route>
-        <Route path="/done" exact>
-          <SuccessPage />
-        </Route>
-      </Switch>
+
       <div>
         <PageBody />
       </div>
--- src/layout/PageBody.js
+++ src/layout/PageBody.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import OrderPage from "../pages/OrderPage";
+import SuccessPage from "../pages/SuccessPage";
 
 function PageBody() {
-  return <div>PageBody</div>;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+      <Route path="/order" exact>
+        <OrderPage />
+      </Route>
+      <Route path="/done" exact>
+        <SuccessPage />
+      </Route>
+    </Switch>
+  );
 }
 
 export default PageBody;
