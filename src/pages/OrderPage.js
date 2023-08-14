97364836eddc1189644a9bcbf702514bcabe68f2
burgokmen <burgokmen@gmail.com>
2023-08-14 23:59:55 +0300

    orderpage ongoing

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function OrderPage() {
-  return <div>OrderPage</div>;
+  return (
+    <div>
+      <h2> Position Absolute Aci Pizza</h2>
+    </div>
+  );
 }
 
 export default OrderPage;
