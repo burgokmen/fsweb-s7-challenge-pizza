5cd91d1209b774415a919550fb8c42620c221b97
burgokmen <burgokmen@gmail.com>
2023-08-14 23:35:58 +0300

    orderPage ongoing

---
+++ src/components/PizzaForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PizzaForm() {
+  return <div>PizzaForm</div>;
+}
+
+export default PizzaForm;
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom/";
+
+const activeNavLink = {
+  fontWeight: 400,
+};
 
 function Header() {
   return (
     <header>
       <h1>Teknolojik Yemekler</h1>
-      <p>Karnin ac, doyuralim asko</p>
+      <div>
+        <nav>
+          <ul className="navlinks">
+            <li>
+              <NavLink to={"/"}> Anasayfa </NavLink>
+            </li>
+            <span> - </span>
+            <li>
+              <NavLink to={"/order"}> Seçenekler </NavLink>
+            </li>
+            <span> - </span>
+
+            <li>
+              <NavLink
+                to={"/order"}
+                style={(isActive) => (isActive ? activeNavLink : null)}
+              >
+                Siparis Olustur
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
